dcc0d01c4fb3b3039b86cc10dacd707e98125d00
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2020-05-14 18:16:07 -0300

    Inicio

---
+++ src/test/java/com/example/mhm_manutencao/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mhm_manutencao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
