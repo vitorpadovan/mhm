dcf4b3fc9fcd360a10b0812e9cc9f2ec5cacf322
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2020-05-14 18:28:23 -0300

    Inicio 2

---
+++ src/main/java/com/br/pcsemdor/mhm_manutencao/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.br.pcsemdor.mhm_manutencao.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class IndexController {
+
+	@GetMapping()
+	public String getIndex() {
+		return "Vitor";
+	}
+}
