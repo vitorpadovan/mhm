55283f106cdbfe3d89733080bc73979ceef61af4
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2020-05-14 19:03:14 -0300

    Comentando as classes antigas

--- src/main/java/com/example/mhm_manutencao/DemoApplication.java
+++ src/main/java/com/example/mhm_manutencao/DemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
-@SpringBootApplication
+//@Controller
+//@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
